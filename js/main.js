c1a273e2fba2afbacd6aea0ece9c870c87d40f72
fort119 <121405144+fort119@users.noreply.github.com>
2022-12-25 23:04:18 +0200

    new site

---
+++ js/main.js
@@ -0,0 +1,36 @@
+﻿$(function(){
+    $('.design__slider').slick({
+      arrows:false,
+      dots: true,
+    });
+});
+
+
+$(function(){
+    $('.flow__slider').slick({
+      arrows:false,
+      dots: true,
+    });
+});
+
+
+$(function(){
+    $('.welding__slider').slick({
+      arrows:false,
+      dots: true,
+    });
+}); 
+
+$(function(){
+    $('.board__slider').slick({
+      arrows:false,
+      dots: true,
+    });
+});
+
+$(function(){
+    $('.polishing-slider').slick({
+      arrows:false,
+      dots: true,
+    });
+}); 
